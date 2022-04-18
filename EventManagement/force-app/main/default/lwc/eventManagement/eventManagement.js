9283a5c5a55c4ee007da53b4baa491d708b612f4
Harshitha-1997 <102682528+Harshitha-1997@users.noreply.github.com>
2022-04-18 06:09:29 +0530

    sample table and some function taken from sites

--- EventManagement/force-app/main/default/lwc/eventManagement/eventManagement.js
+++ EventManagement/force-app/main/default/lwc/eventManagement/eventManagement.js
@@ -36,4 +36,40 @@ export default class EventManagement extends LightningElement {
         this.eventDetails = '';
     } 
 
+    //function to reset all values when reset button is clicked
+    resetAll(){
+        this.eventId = '';
+        this.eventDetails = '';
+        this.isEventAvailable = false;
+    }
+
+    // function to return some hard-coded events name and Id
+    // Taken From https://developer.salesforce.com/docs/component-library/bundle/lightning-combobox/example : Salesforce Licence
+    get samplesEvent() {
+        return [
+            { label: 'Northern Vegan Festival 2022', value: '254539593777' },
+            { label: 'The Pink Picnic 2022', value: '253332112167' },
+            { label: 'Swiftogeddon - The Taylor Swift Club Night', value: '214703783797' },
+            { label: 'Manchester Careers Fair', value: '227566145467' },
+            { label: 'Haigh Dino Days', value: '256736956147' },
+            { label: 'MANCHESTER IFTAR DINNER EVENT 2022 (WEDNESDAY 27TH APRIL 2022)', value: '277720368097' },
+            { label: 'Big Fish Little Fish x Camp Bestival - Manchester Family Rave - Mark XTC', value: '264137290747' },
+            { label: 'Manchester Vintage Kilo Sale', value: '298263583407' },
+            { label: 'Macclesfield Craft Beer & Gin Festival - 6th & 7th May 2022', value: '298774621937' },
+            { label: 'Manchester Warehouse Worship', value: '291352211307' },
+            { label: 'Etihad Stadium Wedding Fayre hosted by County Brides', value: '167945897701' },
+            { label: 'KLUB KIDS UK presents DRAG FEST MANCHESTER 2022 (ages 18+)', value: '232550493787' },
+            { label: 'Achieving Net Zero in Housing Live Event (Sponsored by WSP)', value: '295311734347' }
+        ];
+    }
+
+    // function to show error message in UI if comes.
+    showMessage(header,type,msg) {
+            const event = new ShowToastEvent({
+                title: header,
+                variant: type,
+                message: msg
+            });
+            this.dispatchEvent(event);
+    }
 }
